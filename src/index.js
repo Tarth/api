1a415fde1e93bd6094f15106d312a9a7d422baf8
MBE <Zirax83@gmail.com>
2020-08-04 18:59:33 +0200

    added CORS package and implemented it in the api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
 const port = 3003;
 const db = require("./queries.js");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -12,8 +14,8 @@ app.use(
 );
 
 app.get("/", async (request, response, next) => {
-  // response.json({ info: "Node.js, Express, and Postgres API" });
-  response.send("Hello World");
+  response.json({ info: "Node.js, Express, and Postgres API" });
+  //response.send("Hello World");
 });
 
 app.get("/workers", db.getAllJobs);
