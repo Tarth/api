3ea8d413b2e7c016bc704517954403fb6a41458b
MBE <zirax83@gmail.com>
2020-08-14 13:01:33 +0200

    added file logging with 1 log per day
    TODO log filenames should be the current date

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
+const rfs = require("rotating-file-stream");
 const port = 3003;
 const db = require("./queries.js");
 
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: path.join(__dirname, "logs"),
+});
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(
+  morgan(":date[web] :remote-addr :method :status :url :user-agent", {
+    stream: accessLogStream,
+  })
+);
 app.use(
   bodyParser.urlencoded({
     extended: true,
