9f11d052c2e8fbf318472c91a0faa23e1414eb32
MBE <zirax83@gmail.com>
2021-06-15 14:01:07 +0200

    created request validation for userauth

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+const userData = require("../users.json");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
@@ -16,7 +18,31 @@ function authenticateToken(req, res, next) {
 function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "45s" });
 }
+
+async function authenticateUser(req, res) {
+  if (
+    !req.body.hasOwnProperty("mail") ||
+    !req.body.hasOwnProperty("password")
+  ) {
+    return "missing mail/password";
+  }
+  const user = userData.find((user) => user.mail == req.body.mail);
+  if (user == undefined) {
+    return "missing user";
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      return user;
+    } else {
+      return "wrong password";
+    }
+  } catch {
+    res.status(500).send();
+  }
+}
+
 module.exports = {
   authenticateToken,
+  authenticateUser,
   generateAccessToken,
 };
